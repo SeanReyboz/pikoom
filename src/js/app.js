8c56d4a5af4eed37442fe761a83532833e3545dc
SeanReyboz <seanreyboz@protonmail.com>
2022-01-19 11:02:10 +0100

    Added page transition

--- src/js/app.js
+++ src/js/app.js
@@ -7,3 +7,58 @@ import { Cursor } from "./cursor.js"
 window.onload = () => {
   const cursor = new Cursor(document.querySelector(".cursor"))
 }
+
+const body = document.querySelector("body")
+const transition = document.querySelector(".page-transition")
+const tl = new TimelineMax()
+
+function delay(n) {
+  return new Promise(done => {
+    setTimeout(() => {
+      done()
+    }, n)
+  })
+}
+
+barba.init({
+  sync: true,
+  transitions: [
+    {
+      // En quittant chaque page
+      async leave() {
+        const done = this.async()
+        tl.to(".page-transition", {
+          top: "0%",
+          height: "100%",
+          duration: 1,
+          transition: "Power3.easeOut",
+        })
+        await delay(1000)
+        done()
+      },
+
+      // En arrivant sur une nouvelle page
+      enter() {
+        tl.to(".page-transition", {
+          top: "100%",
+          height: "100%",
+          duration: 0.6,
+          delay: 0.3,
+          transition: "Power3.easeOut",
+        }).set(".page-transition", { top: "0%", height: "0%" })
+      },
+
+      // Avant chaque transition
+      before() {
+        body.classList.add("no-scroll")
+      },
+
+      // Après chaque transition
+      after() {
+        body.classList.remove("no-scroll")
+        // Recréer un curseur
+        return new Cursor(document.querySelector(".cursor"))
+      },
+    },
+  ],
+})
